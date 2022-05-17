d7e63ae6857a24150b977ae0575dbcc3f03e20fc
Patrick Young <patrickyoung281@gmail.com>
2022-05-17 11:47:39 -0400

    commit tree f5d5a20c7f31b5d01e340a1cb075e34495161da1

--- index.js
+++ index.js
@@ -1,1 +1,99 @@
-// code your solution here
+
+
+
+
+
+function superbowlWin (array) {
+    let answer = array.find(record => record.result === "W")
+    if (answer) {
+        return answer.year;
+    }
+    else return undefined;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+function superbowlWin (array) {
+    let answer = array.find(record => record.result === "W")
+    if (answer) {
+        return answer.year;
+    }
+    else return undefined;
+};
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+function superbowlWin (array) {
+    const win = array.find(gameObj => gameObj.result === "W")
+    if (win) {
+        return win.year;
+    }
+    else return undefined
+    };
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
